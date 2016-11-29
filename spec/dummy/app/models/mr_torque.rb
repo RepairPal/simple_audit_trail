e31ff855cf1761b8afd8ed790e09691b5ad7e5d5
Christopher Maujean <cmaujean@repairpal.com>
2016-11-29 14:04:48 -0800

    convert dummy app to rails 4

--- spec/dummy/app/models/mr_torque.rb
+++ spec/dummy/app/models/mr_torque.rb
@@ -1,4 +1,3 @@
 class MrTorque < ActiveRecord::Base
-  attr_accessible :todays_quote
   audit [:todays_quote], :require_audited_user_id => false
 end
--- spec/dummy/app/models/tina.rb
+++ spec/dummy/app/models/tina.rb
@@ -1,4 +1,3 @@
 class Tina < ActiveRecord::Base
-  attr_accessible :badonkadonks, :ladies, :mushy_snugglebites
   audit [:ladies, :badonkadonks]
 end
